b14b260b11fd63b81ff2657126c2e799d9178842
Chaikew <68963282+Chaikew@users.noreply.github.com>
2023-02-23 22:30:01 +0100

    Update jump.h
    - Added doxygen doc
    - Removed useless macro

--- src/jump.h
+++ src/jump.h
@@ -1,10 +1,12 @@
 #ifndef __JUMP_H
 #define __JUMP_H
 
-#define NOP_PADDING(fn) \
-    __asm__ __volatile__ ( \
-        "nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;" \
-    );
-
+/**
+ * @brief Overwrites the first *jump code size* bytes of a function with a jump to the target code.
+ * @param at The function to overwrite.
+ * @param to The target code (0xdeadbeefdeadbeef).
+ * @return 0 on success, -1 on failure.
+ */
 int InsertJump(void* at, void* to);
+
 #endif
